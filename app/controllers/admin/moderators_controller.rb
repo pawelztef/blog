740eb3d5396c48816853903c4e902d3d836a606d
pawelztef <pawelztef@gmial.com>
2016-10-08 00:54:42 +0100

    edit moderator implemented lesson 12

--- app/controllers/admin/moderators_controller.rb
+++ app/controllers/admin/moderators_controller.rb
@@ -2,4 +2,9 @@ class Admin::ModeratorsController < ApplicationController
   def index
     @moderators = Moderator.all
   end
+  def edit
+    @moderator = Moderator.find(params[:id])
+  end
+  def update
+  end
 end
