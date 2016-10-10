d81c741c6c2c1cc536f5cb3b1cc136b51112d8b4
pawelztef <pawelztef@gmial.com>
2016-10-10 17:55:24 +0100

    posts controller added, routes customized lesson 20

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/logout' => 'admin/sessions#destroy'
 
   namespace :admin do 
+    resources :posts
     resources :sessions, only: [:new, :create, :destroy]
     resources :moderators, only: [:index, :edit, :update]
   end
