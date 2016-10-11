c299821c3d53dc316576708ea77fb0beae8eb0d0
pawelztef <pawelztef@gmial.com>
2016-10-11 16:47:58 +0100

    lesson 24

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,6 +1,11 @@
 class Admin::PostsController < Admin::ApplicationController
+
   def index
+    if params[:search].present?
+    @posts = Post.where("title LIKE ? OR content LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%").page params[:page]
+    else
     @posts = Post.all.order(id: :desc).page params[:page]
+    end
   end
 
   def new
