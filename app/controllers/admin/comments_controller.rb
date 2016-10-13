5e50f8bbf1c1ecf47099f002621d97224ae25275
pawelztef <pawelztef@gmial.com>
2016-10-13 13:19:58 +0100

    lesson 36 finished search comments etc.

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,13 +1,23 @@
 class Admin::CommentsController < Admin::ApplicationController
   def index
-    @comments = Comment.where(status: to_bool(params[:status]))
+    if params[:search].present?
+      @comments = Comment.joins(:visitor).where("fullname LIKE ? OR message LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%").page params[:page]
+    else
+      @comments = Comment.where(status: to_bool(params[:status])).page params[:page]
+    end
   end
 
   def update
+    @comment = Comment.find(params[:id])
+    if @comment.update_attributes(status: params[:status])
+      redirect_to :back, notice: "Comment sucessfully updated"
+    else
+      redirect_to :back, notice: "There was a problem updating commnet"
+    end
   end
 
   def destroy
     Comment.destroy(params[:id])
-    redirect_to admin_comments_url, notice: "Comment deleted succesfully"
+    redirect_to :back, notice: "Comment deleted succesfully"
   end
 end
