32692d0cf5969c5d47fc01160d59b6a4fe28e3d5
pawelztef <pawelztef@gmial.com>
2016-10-15 00:51:46 +0100

    left on lesson 45 2min45sec notifications/index

--- app/controllers/admin/notifications_controller.rb
+++ app/controllers/admin/notifications_controller.rb
@@ -1,5 +1,7 @@
 class Admin::NotificationsController < Admin::ApplicationController
   def index
+    @visitor_notifications = Notifications.where(notifiable_type: 'Visitor').order(id: :desc)
+    @comment_notifications = Notifications.where(notifiable_type: 'Comment').order(id: :desc)
   end
 
   def destroy
