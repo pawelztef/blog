f698f657a135e0e18f0c1509a5835ec77008cbf2
pawelztef <pawelztef@gmial.com>
2016-10-16 14:17:05 +0100

    lesson 55 comment form nestet attributes **important

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,10 @@
 class CommentsController < ApplicationController
   def create
+    @visitor = Visitor.new(visitor_comment_params).save
+    redirect_to :back
+  end
+  private
+  def visitor_comment_params
+    params.require(:visitor).permit(:fullname, :email, :comments_attributes => [:message, :post_id])
   end
 end
