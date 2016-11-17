c54d177867ad3d0ce57fcb55bd81183e7d1f6ad4
pawelztef <pawelztef@gmial.com>
2016-11-17 11:40:21 +0000

    lesson 20

--- app/helpers/admin/tags_helper.rb
+++ app/helpers/admin/tags_helper.rb
@@ -2,11 +2,11 @@ module Admin::TagsHelper
   def create_deletable_button(tag)
     if tag.in_use? 
       link_to '#' do
-        content_tag(:button, "Delete", class: "disabled")
+        content_tag(:span, "Delete", class: "btn btn-xs btn-default disabled")
       end
     else
       link_to admin_tag_path(tag), method: :delete do
-        content_tag(:button, "Delete")
+        content_tag(:span, "Delete", class: "btn btn-xs btn-default")
       end
     end
   end
