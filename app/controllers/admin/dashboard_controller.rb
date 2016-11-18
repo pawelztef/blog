13e836f68618ef185e24aa7a5c5fcc52f47d9d46
pawelztef <pawelztef@gmial.com>
2016-11-18 01:23:15 +0000

    tutorial finished

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class Admin::DashboardController < Admin::ApplicationController
   def index
-    @posts = Post.last 5
-    @comments = Comment.last 5
+    @posts = Post.last 8
+    @comments = Comment.last 3
     @visitors = Visitor.last 5
     @messages = 5
     @notifications = 5
