b0589432ea16458e322c58d24b5e22d819de4039
pawelztef <pawelztef@gmial.com>
2016-11-20 12:09:59 +0000

    finishing styles in dashboard after changing views from tables to cols lists

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class Admin::DashboardController < Admin::ApplicationController
   def index
-    @posts = Post.last 8
+    @posts = Post.last 10
     @comments = Comment.last 3
     @visitors = Visitor.last 5
     @messages = 5
