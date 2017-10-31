e623e99946536a0cfc299e8d3f3c779e4dcc2e1c
pawelztef <pawelztef@gmail.com>
2017-10-31 09:30:44 +0000

    styles icons uploads

--- app/controllers/admin/social_modules_controller.rb
+++ app/controllers/admin/social_modules_controller.rb
@@ -2,6 +2,7 @@ class Admin::SocialModulesController < Admin::ApplicationController
   before_action :set_admin_social_module, only: [:index, :show, :edit, :update, :destroy]
 
   def index
+    #TODO clean all actions
   end
 
   def show
@@ -36,6 +37,5 @@ class Admin::SocialModulesController < Admin::ApplicationController
 
   def social_module_params
     params.require(:social_module).permit(social_links_attributes: SocialLink.attribute_names.map(&:to_sym).push(:_destroy))
-    # params.require(:social_module).permit(:something)
   end
 end
--- app/models/social_link.rb
+++ app/models/social_link.rb
@@ -1,5 +1,6 @@
 class SocialLink < ActiveRecord::Base
   belongs_to :social_module
+  mount_uploader :icon, IconUploader
   validates :icon, presence: true
   validates :url, presence: true
 end
