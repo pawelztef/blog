a9818797e713897da802a9902e36c1fa7707783f
pawelztef <pawelztef@gmail.com>
2017-11-01 05:03:32 +0000

    social module links updated

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get '/logout' => 'admin/sessions#destroy'
 
   namespace :admin do 
-    resources :social_modules, path: "social_module"
+    resources :social_modules, path: "social_module", only: [:index, :edit, :udate, :destroy]
     resources :posts
     resources :books do
       collection do
