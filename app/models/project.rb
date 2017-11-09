bca66c7dab9ec0588260e21401bc87611ee698a2
pawelztef <pawelztef@gmail.com>
2017-11-09 05:49:42 +0000

    join table tags and projects added association implemented to views

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,9 @@
 class Project < ActiveRecord::Base
   has_and_belongs_to_many :project_images
+  has_and_belongs_to_many :tags
   validates :title, presence: true 
   validates :description, presence: true
   accepts_nested_attributes_for :project_images
+  accepts_nested_attributes_for :tags
 
 end
