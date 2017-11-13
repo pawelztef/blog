3856253cf0f9e05d6fe590897896448e3a5dee84
pawelztef <pawelztef@gmail.com>
2017-11-13 07:56:20 +0000

    calibration of dim effect

--- app/assets/javascripts/front/portfolio.js
+++ app/assets/javascripts/front/portfolio.js
@@ -1,6 +1,6 @@
 $(function() {
   var tag = $('#tags-2');
-  var range = 330;
+  var range = 350;
   $(window).on('load scroll', function () {
     $('.project').each(function() {
       dimElement($(this), range);
@@ -10,7 +10,7 @@ $(function() {
 
 
 function dimElement(elem, range) {
-  var elemPos = Math.abs(((elem.offset().top+(elem.outerHeight()/2)) - ($(window).scrollTop() + $(window).outerHeight()/2))),
+  var elemPos = Math.abs(((elem.offset().top+(elem.outerHeight()/2)) - ($(window).scrollTop() - 50 + $(window).outerHeight()/2))),
     calc2 = -(elemPos - range) / range;
 
   elem.css({ 'opacity': calc2 });
